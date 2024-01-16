0395574a9f5a95fd8d61cda1053e030a4a2f5b45
HakanSezer <hakanszer1@gmail.com>
2024-01-16 11:47:06 +0300

    New

---
+++ FilmKitabı/FilmKitab_App.swift
@@ -0,0 +1,17 @@
+//
+//  FilmKitab_App.swift
+//  FilmKitabı
+//
+//  Created by Hakan Sezer on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FilmKitab_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
